a75db9286d97bee418772e902a0f8f3aae7f83ba
rmd09 <rmd09@yandex.ru>
2024-12-04 21:10:22 +0300

    Плавный старт

--- Code/Code.ino
+++ Code/Code.ino
@@ -1,17 +1,18 @@
 #define dW digitalWrite
 #define dR digitalRead
 
+
 //Пины
 //in - input на драйвере l293d
 //en - enable
-#define in1 0
-#define in2 0
-#define in3 0
-#define in4 0
-#define en1 0
-#define en2 0
+#define in1 13
+#define in2 12
+#define in3 11
+#define in4 10
+#define en1 5
+#define en2 6
 
-#define debug_btn 0 //Кнопка на макетке для отладки
+#define debug_btn 7 //Кнопка на макетке для отладки
 
 void init_motors()
 {
@@ -30,43 +31,58 @@ void init_debug_components()
 
 void setup() 
 {
+  delay(2000);
   init_motors();
   init_debug_components();
 
+
 }
 
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
+/***************************CONST*****************************/
+const int B_max = 255;
+const int A_max = 225;
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
 
-void A_on(int en_value = 1023, bool isStraight = true)
+void B_on(int en_value = B_max, bool isStraight = true)
 {
   digitalWrite(in1, isStraight ? HIGH : LOW);
   digitalWrite(in2, isStraight ? LOW : HIGH);
-  digitalWrite(en1, en_value);
-}
-void A_off()
-{
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(en1, LOW);
-}
-void B_on(int en_value = 1023, bool isStraight = true)
-{
-  digitalWrite(in3, isStraight ? LOW : HIGH);
-  digitalWrite(in4, isStraight ? HIGH : LOW);
-  digitalWrite(en2, en_value);
+  analogWrite(en1, en_value);
 }
 void B_off()
 {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(en1, LOW);
+}
+void A_on(int en_value = A_max, bool isStraight = true)
+{
+  digitalWrite(in3, isStraight ? LOW : HIGH);
+  digitalWrite(in4, isStraight ? HIGH : LOW);
+  analogWrite(en2, en_value);
+}
+void A_off()
+{
   digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
+  digitalWrite(in4, HIGH);
   digitalWrite(en2, LOW);
+}
+void Start(int t)
+{
+  for (int i = B_max/3, j = A_max/3; i < B_max; i++, j++)
+  {
+    delay(t/(A_max/3));
+    A_on(j);
+    B_on(i);
+  }
 }
 
 void test_program()
 {
-  A_on();
-  B_on();
+  Start(5000);
   delay(2000);
   A_off();
   B_off();
@@ -77,6 +93,7 @@ void test_program()
 
 void loop() 
 {
+
   bool but = !digitalRead(debug_btn);
   if (but) 
   {
